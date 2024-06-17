81f4324c5e637b7fa00b2eef67f105bf2ac745d3
3shigami <115579067+3shigami@users.noreply.github.com>
2024-06-17 23:56:23 +0300

    Update main.py

--- lesson/main.py
+++ lesson/main.py
@@ -37,6 +37,133 @@ async def cmd_start(message: types.Message):
     #любой код
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+from aiogram import Bot, executor, Dispatcher, types
+from scripts.markup_janre import Markup_janre
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+
+API_TOKEN = "7456660156:AAE7aZ_N_7VCxbrfIc7R54wEgPY54buKRRA"
+
+#создаем самого бота
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+class AwaitMessages(StatesGroup):
+    janre = State()
+
+
+
+
+
+#создаем событие которое будет отвечать за /start
+@dp.message_handler(commands=['start'])
+#функция куда попадает сообщение нашего пользователя types.Message - наше сообщение
+async def cmd_start(message: types.Message):
+    #то что бот будет отвечать на /start
+    #любой код
+    
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    markup.add(types.InlineKeyboardButton("Медный бык", callback_data="1488"))
+    await message.answer("пасхалко", reply_markup=markup)
+    #любой код
+
+
+@dp.message_handler(commands=['janre'])
+#функция куда попадает сообщение нашего пользователя types.Message - наше сообщение
+async def janre_start(message: types.Message):
+    #то что бот будет отвечать на /start
+    #любой код
+    print(123)
+    await message.answer("Вот список жанров", reply_markup=Markup_janre.get_markup())
+    #любой код
+    await AwaitMessages.janre.set()
+
+
+
+@dp.callback_query_handler(state=AwaitMessages.janre) 
+async def janre(call:types.CallbackQuery, state: FSMContext):
+    async with state.proxy() as data:
+      await call.message.edit_text(f"Вы выбрали '{call.data}' как жанр")
+      await state.finish()
+
+
+
+
+@dp.message_handler()
+async def answer(message: types.Message):
+    print(message.text)
+
+
+@dp.callback_query_handler(text_startswith="1488") 
+async def registration(call:types.CallbackQuery):
+    print(call)
+    """{"id": "4033832339526762352",
+      "from": {"id": 939199780, "is_bot": false, "first_name": "eiancbennx", "username": "ehxnekcnejxkexn", "language_code": "ru"},
+        "message": {"message_id": 134,
+          "from": {"id": 7456660156, "is_bot": true,
+            "first_name": "lesson_roma", "username": "lesson_roma_bot"},
+              "chat": {"id": 939199780, "first_name": "eiancbennx", "username": "ehxnekcnejxkexn",
+                "type": "private"}, "date": 1718588320, "text": "пасхалко",
+                  "reply_markup": {"inline_keyboard": [[{"text": "Медный бык", "callback_data": "1488"}]]}},
+                    "chat_instance": "7368618751707300189", "data": "1488"}"""
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    markup.add(types.InlineKeyboardButton("Пасхалко", callback_data="1"))
+    await call.message.edit_text("медный бык был нажат", reply_markup=markup)
+
+@dp.callback_query_handler(text_startswith="1") 
+async def registration(call:types.CallbackQuery):
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    markup.add(types.InlineKeyboardButton("Джокер", callback_data="2"))
+    await call.message.edit_text("пасхалко был нажат", reply_markup=markup)
+
+
+@dp.callback_query_handler(text_startswith="2") 
+async def registration(call:types.CallbackQuery):
+    await call.message.edit_text("Джоекер был нажат")
+
+
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
+
+
+"""1)при /start у человека появляется набор из следующих инлайн кнопок 1)1-10 2)10-15 3)15-20 при сообщении введите ваш возраст, и потом бот редактирует сообщение и пишет, ваш возраст и то что он выбрал"""
+
+
+
+
+
+
 @dp.message_handler()
 async def answer(message: types.Message):
     print(message.text)
